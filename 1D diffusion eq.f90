702c33cc3d69fd97cbd086e5b944c8be671cd378
Carlos Rubio <86541435+charlesrt97@users.noreply.github.com>
2022-01-06 00:06:53 -0600

    Update 1D diffusion eq.f90

--- 1D diffusion eq.f90
+++ 1D diffusion eq.f90
@@ -1,13 +1,12 @@
 ! Solves the 1-dimensional diffusion equation, using a finite difference method
 
-! sujeta a condicion inicial: T(x,0)=20ºC (en todo el alambre),
-! condiciones de frontera: extremo izquierdo T(1,0)=60ºC, derecho T(nx,0)=20ºC
+! with the initial condition: T(x,0)=20ºC
+! boundary conditions: left boundary T(1,0)=60ºC, right boundary T(nx,0)=20ºC
 
-! definicion de variables
-program difusion
+program diffusion
 implicit none
 
-! definicion de variables
+! sets up variables used
 integer :: nx, i, tt, lx, xl, i_sample,n_serie,it
 real :: lambda, dx, nx1
 real, allocatable, dimension (:) :: T,T0,x
@@ -16,10 +15,10 @@ character(16) outputfield
 character(2) serie
 character(3) sample
 
-nx=100.0 ! tamaño de la malla
-nx1=100.0 ! tamaño de la malla flotante
-lx=10.0; ! longitud del alambre
-xl=0 ! coordenada fisica extremo izquierdo
+nx=100.0 ! mesh size
+nx1=100.0 ! mesh size (float)
+lx=10.0; ! wire's length
+xl=0 ! left physical coordinate
 lambda=0.4
 
 i_sample=0
@@ -30,20 +29,20 @@ allocate(T(nx))
 allocate(T0(nx))
 allocate(x(nx))
 
-! condiciones iniciales
+! initial conditions
 do i=1,nx
   T0(i)=20.0
 end do
 
-! tiempo
+! time
 do tt=1,1000
 
-  ! vector x (posicion)
+  ! position vector, x
   do i=1,nx
     x(i)=xl+i*lx/(nx1)
   end do
 
-  ! escribir archivos cada 100 unidades de tiempo
+  ! writes to disk 100 files
   if (it.eq.100) then
     i_sample=i_sample+1
     write(sample,'(i3.3)')i_sample
@@ -59,16 +58,15 @@ do tt=1,1000
     it=it+1
   end if
 
-  ! metodo explicito
+  ! explicit method
   do i=2,nx-1
     T(i)=lambda*T0(i-1)+(1.0-2.0*lambda)*T0(i)+lambda*T0(i+1)
   end do
 
-  ! aplicamos condiciones de frontera
+  ! applies boundary conditions
   T(1)=60.0
   T(nx)=20.0
-
-  ! sobreescribe T a T0. Para tener a T0 como T^(n), y a T como T^(n+1)
+  
   do i=1,nx
     T0(i)=T(i)
   end do
@@ -77,4 +75,4 @@ end do
 
 100 format(f12.8,f12.8)
 
-end program difusion
+end program diffusion
